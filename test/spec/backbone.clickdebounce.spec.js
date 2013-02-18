75a6d57d4af92d56455e244f607bac2ae06c626e
alexander.beletsky <alexander.beletsky@gmail.com>
2013-02-18 17:24:38 +0200

    simple cases of non-debounced view

--- test/spec/backbone.clickdebounce.spec.js
+++ test/spec/backbone.clickdebounce.spec.js
@@ -1,7 +1,57 @@
 describe('Backbone.ClickDebounce spec', function () {
 
-    it ('should pass', function () {
-        expect(1).to.equal(1);
+    describe('when using non-debounced view', function () {
+        var view;
+
+        beforeEach(function () {
+            var View = Backbone.View.extend({
+                initialize: function () {
+                    this.clickCounter = 0;
+                },
+
+                'events': {
+                    'click .click-me': 'onClick'
+                },
+
+                render: function () {
+                    this.$el.html('<a href="#" class="click-me">Click me</a>');
+                },
+
+                onClick: function () {
+                    this.clickCounter++;
+                }
+            });
+
+            view = new View();
+            view.render();
+        });
+
+        it ('should view be created', function () {
+            expect(view).to.be.ok;
+        });
+ 
+        describe('when user clicks to button', function () {
+            beforeEach(function () {
+                view.$el.find('.click-me').click();
+            });
+
+            it('should increase counter', function () {
+                expect(view.clickCounter).to.equal(1);
+            });
+
+            describe('when clicking multiple times', function () {
+                beforeEach(function () {
+                    view.$el.find('.click-me').click();
+                    view.$el.find('.click-me').click();
+                    view.$el.find('.click-me').click();
+                });
+
+                it('should increase counter', function () {
+                    expect(view.clickCounter).to.equal(4);
+                });
+            });
+
+        });
     });
     
 });
